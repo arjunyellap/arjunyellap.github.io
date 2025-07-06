81a532a8a29b5861da26233bee072b62968a269e
arjunyellap <arjunakamessi@gmail.com>
2025-07-06 12:48:32 +1000

    Add files via upload

---
+++ Arbritrage.py
@@ -0,0 +1,29 @@
+print('Hello! This is an arbitrage calculator.')
+print('Here, I am considering 2 bookmakers with 2 odds.')
+win1=input('Give the winning odd on betting site 1: ')
+lose1=input('Give the losing odd on betting site 1: ')
+win2=input('Give the winning odd on betting site 2: ')
+lose2=input('Give the losing odd on betting site 2: ')
+
+bestwin = float(max(win1, win2))
+bestloss = float(max(lose1, lose2))
+
+arbitrage = ((1/bestwin) + (1/bestloss))
+score=(100-(arbitrage*100))
+if score<0:
+   print('There is no possible arbitrage with these odds.')
+
+else:
+   print(score,'% is our profit margin.')
+   amount = float(input('How much are you looking to bet? '))
+   profit = (amount/arbitrage) - amount
+   percentageprofit = (profit/amount)*100
+   print('$',profit,'is your guaranteed profit, or a return of',percentageprofit,'%.')
+   winstake = (amount*(1/bestwin))/arbitrage
+   lossstake = (amount*(1/bestloss))/arbitrage
+
+   print('Place a bet of $',winstake,'for a win.')
+   print('Place a bet of $',lossstake,'for a loss.')
+
+
+
